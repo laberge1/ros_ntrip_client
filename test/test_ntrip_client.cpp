72a644c7f5711b904cdc79cf5e6a0c115baf4f90
Ollie Walsh <ollie.walsh@gmail.com>
2026-03-31 04:59:27 +0100

    Add TLS success-path regression tests

--- test/test_ntrip_client.cpp
+++ test/test_ntrip_client.cpp
@@ -11,6 +11,9 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#include <openssl/err.h>
+#include <openssl/ssl.h>
+
 #include <array>
 #include <atomic>
 #include <chrono>
@@ -495,6 +498,237 @@ const char kTestCertificatePem[] =
     "TA70vsK1tnE+bBZ5qTqL0U8nyCLlcQFV AiEAxgN9zeps2sonMSKcwk5Y8ZndKyV+\n"
     "XS6/4FwXk4Hknc0=\n"
     "-----END CERTIFICATE-----\n";
+
+const char kTlsServerCertificatePem[] =
+    "-----BEGIN CERTIFICATE-----\n"
+    "MIIDIzCCAgugAwIBAgIUMizRldFjUTo5JyPAr3LxAkf0UlAwDQYJKoZIhvcNAQEL\n"
+    "BQAwFDESMBAGA1UEAwwJbG9jYWxob3N0MB4XDTI2MDMzMTAzNTIyMloXDTM2MDMy\n"
+    "ODAzNTIyMlowFDESMBAGA1UEAwwJbG9jYWxob3N0MIIBIjANBgkqhkiG9w0BAQEF\n"
+    "AAOCAQ8AMIIBCgKCAQEAqmRNQzjbyRu2UfXrh4ofgAR7tv4eaw65AerIEvxTI7zE\n"
+    "dMs1Pj8TgzhshSb8FM6zprBC9uxQ+GCzlMEzaXyqxDa73ovqJj/4r9j0xqv8Dl2C\n"
+    "4+iYIAjmbxkYnD4Yt+OaFKxraGyEljdOq4ZLbiegYf5yfFo3fVt4q9UT0xGd7RtT\n"
+    "Ak7kkZDBVoFpxrenp4ltLpV/ymTs6akoMZzrmZDhFxRrsJFqs4cmW4UaFF6otSGb\n"
+    "/452E/14RMEwLEOA9D4lGayqg9AJA1D9wCLtsLO39gvtqwDAzf16Q2sxqqQ/8ni/\n"
+    "3yx4Ta+pIsDxZrUiXkKY5co74SZAnG61YqY2/Oep7wIDAQABo20wazAUBgNVHREE\n"
+    "DTALgglsb2NhbGhvc3QwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMCAqQw\n"
+    "EwYDVR0lBAwwCgYIKwYBBQUHAwEwHQYDVR0OBBYEFISmvw2MX4eUXuogSA8vsdU/\n"
+    "npcsMA0GCSqGSIb3DQEBCwUAA4IBAQCWrx8gIuZo3IMo9NCwwlChpj/LV0eld8Tk\n"
+    "XtiI+Ct49CMgk8fo3YzodXZIoWkQ2wYTxmH7ehvebMDJsVQnV9hZCQDVOZSoJor9\n"
+    "QbMi5ZiZLFWq4e9OeImR7LPPq1BqDxLol/CZLNf4T/5lXh5+oD8gY0cRZh/DqFUg\n"
+    "frTmr4GIZJ4aZj3CSpExZ9doZBP273FQIu/qCeVrpCJTFh1CGXaIdCVDh7HEzCuB\n"
+    "MyAeONIczINndZDgjYRAVm4LVyQLJyZ3KMXKYvrHso7TagGqG0EWAX2yZpFL8fHW\n"
+    "Ja9Ubdf3RyY/oqivgH/Yc9k6hoj0KQpFlXiPZVSyGtruqnbWp3/+\n"
+    "-----END CERTIFICATE-----\n";
+
+const char kTlsServerPrivateKeyPem[] =
+    "-----BEGIN PRIVATE KEY-----\n"
+    "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCqZE1DONvJG7ZR\n"
+    "9euHih+ABHu2/h5rDrkB6sgS/FMjvMR0yzU+PxODOGyFJvwUzrOmsEL27FD4YLOU\n"
+    "wTNpfKrENrvei+omP/iv2PTGq/wOXYLj6JggCOZvGRicPhi345oUrGtobISWN06r\n"
+    "hktuJ6Bh/nJ8Wjd9W3ir1RPTEZ3tG1MCTuSRkMFWgWnGt6eniW0ulX/KZOzpqSgx\n"
+    "nOuZkOEXFGuwkWqzhyZbhRoUXqi1IZv/jnYT/XhEwTAsQ4D0PiUZrKqD0AkDUP3A\n"
+    "Iu2ws7f2C+2rAMDN/XpDazGqpD/yeL/fLHhNr6kiwPFmtSJeQpjlyjvhJkCcbrVi\n"
+    "pjb856nvAgMBAAECggEAEGmaP5q0dkn4ZStTmo9QEBwJG/ac+3bu6c5cTefcIkRx\n"
+    "L/F67sSonn/K2YXdy4CgMV19Cx0hgisztmUTDLAjdW7y6FwrG7AXG5PBpXlfeArD\n"
+    "LCzKqnCbsaEwi6w3jqRFF9oGe90oEEgXiHy7u0qPL6E7adr5dAEMPeZHrecJLT4c\n"
+    "7x1yLuWcoTgcU/VNY93qmRKfGVpmuIRp+8lOzE1IP/xOMCTHAriXPJ77OQDgTPYS\n"
+    "CNXupr9i7mvnnGrw3gzS4uQ/Novejnx8tlRrbnp7mdQrmZ9r2UH20Uqwl+u1Fp2T\n"
+    "C81aAoP45QLO68jcWQ1Fr2CHgBuvwHXgh+eki1AAwQKBgQDaUKXFOp71AsMaV7Q1\n"
+    "rty7wPt1cg0eDMKuhaGmKRP8Des0s7oB7q3qI6GrPP5MnObhhAZqP0Ag0wCy51YN\n"
+    "aovbartfFURLNKkVFIyLNzzOanDC9847bjWPTEeCpMSW5Z0/rVMFpFo08vYc8AHI\n"
+    "9WG8OHgM/AMEnu6ZgFdijc6rMQKBgQDHzeyoo7T10fFKbeSY1APx8vIkdmZZp0UN\n"
+    "cLG180hlbjKP47aYZiHM6u+vujN0burhBoy+gjm341k9NXfEcTb3sir7/wHXnJ/w\n"
+    "V5+BXeREnZgUs4+77nSQZji41tHN4vq9lz5oCrd7Vpm1gstZ+yEYgeuTFxQLx6Iw\n"
+    "CMywdwYfHwKBgBQpBmXkN/GgQ6wXFUkv0Kp188KwuY1g2EmNhZP2jyXjkyjWwAKV\n"
+    "q/HHQJKzmzgv9RI2QvFkzeM6GQJsYoHyqN+mR49MQ3Y+cq7DqwbgHvlg0vDuOVk7\n"
+    "oC6PeLsTxCO7KH5M3zHSL5JcLWYWs9N+9XuQK2Xnj8/JbXc3ZtpvBfrBAoGAYyaT\n"
+    "TFSA1oPqY28JQ7Xih0xyURnYTKEvKS5FYe9qe7slqDXuRM5Z86CadO/H3P213Rks\n"
+    "+tUQ42oUvMUtu/QavOxTXF39ggudat1wr/fx8QLrl0pMB4ybl5TCjSc9UhhTKYZG\n"
+    "V8cJqlEdUe+oDe0LTWgbiRCox/e3SnLzcJaAGS0CgYEAybZVFK4v6PAmVM8kH5WX\n"
+    "00ZhUlmrdSvSuAY2L/LGWgBDAJaM6G/DZ4i3+HIPaMkkSFFzxpVsW7QVpcT1c0Zq\n"
+    "JbJ9cgLjcTbTaAwmvoqHRkWNjodqQxBJLnQAAj0Vg/Lm4joK2Q9+M+kO/wrqEbfm\n"
+    "5lJKZsJ7odjgXdKa3i4MXgg=\n"
+    "-----END PRIVATE KEY-----\n";
+
+class TlsMockCaster
+{
+public:
+  using Handler = std::function<void(SSL* ssl)>;
+
+  TlsMockCaster(const std::string& certificate_pem,
+                const std::string& private_key_pem,
+                Handler handler)
+    : cert_file_(certificate_pem),
+      key_file_(private_key_pem),
+      handler_(std::move(handler))
+  {
+    static std::once_flag openssl_once;
+    std::call_once(openssl_once, []()
+                   {
+                     SSL_library_init();
+                     SSL_load_error_strings();
+                     OPENSSL_init_ssl(0, nullptr);
+                   });
+
+    ssl_ctx_ = SSL_CTX_new(TLS_server_method());
+    if (ssl_ctx_ == nullptr)
+    {
+      throw std::runtime_error("failed to create TLS server context");
+    }
+
+    if (SSL_CTX_use_certificate_chain_file(ssl_ctx_, cert_file_.path().c_str()) != 1)
+    {
+      throw std::runtime_error("failed to load TLS server certificate");
+    }
+    if (SSL_CTX_use_PrivateKey_file(ssl_ctx_, key_file_.path().c_str(), SSL_FILETYPE_PEM) != 1)
+    {
+      throw std::runtime_error("failed to load TLS server private key");
+    }
+    if (SSL_CTX_check_private_key(ssl_ctx_) != 1)
+    {
+      throw std::runtime_error("TLS server certificate and private key do not match");
+    }
+
+    listen_fd_ = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_fd_ < 0)
+    {
+      throw std::runtime_error("failed to create TLS listen socket");
+    }
+
+    int enable = 1;
+    if (setsockopt(listen_fd_, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(enable)) != 0)
+    {
+      throw std::runtime_error("failed to set TLS SO_REUSEADDR");
+    }
+
+    sockaddr_in address{};
+    address.sin_family = AF_INET;
+    address.sin_port = htons(0);
+    address.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    if (bind(listen_fd_, reinterpret_cast<sockaddr*>(&address), sizeof(address)) != 0)
+    {
+      throw std::runtime_error("failed to bind TLS listen socket");
+    }
+    if (listen(listen_fd_, 1) != 0)
+    {
+      throw std::runtime_error("failed to listen on TLS mock caster");
+    }
+
+    socklen_t address_len = sizeof(address);
+    if (getsockname(listen_fd_, reinterpret_cast<sockaddr*>(&address), &address_len) != 0)
+    {
+      throw std::runtime_error("failed to query TLS mock caster port");
+    }
+    port_ = ntohs(address.sin_port);
+
+    server_thread_ = std::thread([this]()
+                                 {
+                                   sockaddr_in client_address{};
+                                   socklen_t client_len = sizeof(client_address);
+                                   const int client_fd = accept(
+                                       listen_fd_,
+                                       reinterpret_cast<sockaddr*>(&client_address),
+                                       &client_len);
+                                   if (client_fd < 0)
+                                   {
+                                     return;
+                                   }
+
+                                   client_fd_ = client_fd;
+                                   SSL* ssl = SSL_new(ssl_ctx_);
+                                   if (ssl == nullptr)
+                                   {
+                                     shutdown(client_fd, SHUT_RDWR);
+                                     close(client_fd);
+                                     client_fd_ = -1;
+                                     return;
+                                   }
+
+                                   SSL_set_fd(ssl, client_fd);
+                                   if (SSL_accept(ssl) == 1)
+                                   {
+                                     handler_(ssl);
+                                     SSL_shutdown(ssl);
+                                   }
+
+                                   SSL_free(ssl);
+                                   shutdown(client_fd, SHUT_RDWR);
+                                   close(client_fd);
+                                   client_fd_ = -1;
+                                 });
+  }
+
+  ~TlsMockCaster()
+  {
+    stop();
+  }
+
+  int port() const
+  {
+    return port_;
+  }
+
+  void stop()
+  {
+    if (listen_fd_ >= 0)
+    {
+      shutdown(listen_fd_, SHUT_RDWR);
+      close(listen_fd_);
+      listen_fd_ = -1;
+    }
+    if (client_fd_ >= 0)
+    {
+      shutdown(client_fd_, SHUT_RDWR);
+      close(client_fd_);
+      client_fd_ = -1;
+    }
+    if (server_thread_.joinable())
+    {
+      server_thread_.join();
+    }
+    if (ssl_ctx_ != nullptr)
+    {
+      SSL_CTX_free(ssl_ctx_);
+      ssl_ctx_ = nullptr;
+    }
+  }
+
+private:
+  TempFile cert_file_;
+  TempFile key_file_;
+  Handler handler_;
+  SSL_CTX* ssl_ctx_{nullptr};
+  int listen_fd_{-1};
+  std::atomic<int> client_fd_{-1};
+  int port_{-1};
+  std::thread server_thread_;
+};
+
+std::string readTlsRequest(SSL* ssl)
+{
+  const int socket_fd = SSL_get_fd(ssl);
+  if (socket_fd >= 0)
+  {
+    timeval timeout{};
+    timeout.tv_sec = 0;
+    timeout.tv_usec = static_cast<suseconds_t>(
+        std::chrono::duration_cast<std::chrono::microseconds>(kRequestReadTimeout).count());
+    setsockopt(socket_fd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
+  }
+
+  std::string request;
+  std::array<char, 1024> buffer{};
+  while (request.find("\r\n\r\n") == std::string::npos)
+  {
+    const int received = SSL_read(ssl, buffer.data(), static_cast<int>(buffer.size()));
+    if (received <= 0)
+    {
+      break;
+    }
+    request.append(buffer.data(), static_cast<std::size_t>(received));
+  }
+  return request;
+}
 
 }  // namespace
 
@@ -1222,6 +1456,130 @@ TEST(NtripClientTest, RejectsPartialMtlsConfiguration)
 
   const std::vector<StatusCode> copied_codes = copyStatusCodes(codes, code_mutex);
   EXPECT_TRUE(containsStatusCode(copied_codes, StatusCode::TlsError));
+}
+
+TEST(NtripClientTest, ConnectsWithVerifiedTlsAndPublishesRtcm)
+{
+  TempFile ca_certificate(kTlsServerCertificatePem);
+  TlsMockCaster caster(
+      kTlsServerCertificatePem,
+      kTlsServerPrivateKeyPem,
+      [](SSL* ssl)
+      {
+        const std::string request = readTlsRequest(ssl);
+        EXPECT_NE(request.find("GET /TEST HTTP/1.1"), std::string::npos);
+
+        const std::string response =
+            "HTTP/1.1 200 OK\r\n"
+            "Ntrip-Version: Ntrip/2.0\r\n"
+            "Content-Type: gnss/data\r\n"
+            "Connection: close\r\n"
+            "\r\n";
+        ASSERT_EQ(SSL_write(ssl, response.data(), static_cast<int>(response.size())),
+                  static_cast<int>(response.size()));
+        ASSERT_EQ(SSL_write(
+                      ssl, kMinimalRtcmFrame.data(), static_cast<int>(kMinimalRtcmFrame.size())),
+                  static_cast<int>(kMinimalRtcmFrame.size()));
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      });
+
+  NtripClientConfig config = makeConfig(caster.port());
+  config.host = "127.0.0.1";
+  config.tls_enabled = true;
+  config.tls_verify_peer = true;
+  config.tls_server_name = "localhost";
+  config.tls_ca_cert_file = ca_certificate.path();
+  NtripClient client(config);
+
+  std::mutex status_mutex;
+  std::vector<std::string> statuses;
+  std::mutex code_mutex;
+  std::vector<StatusCode> codes;
+  std::mutex frame_mutex;
+  std::vector<std::vector<std::uint8_t>> frames;
+
+  ASSERT_TRUE(client.start(
+      [&](const std::vector<std::uint8_t>& frame)
+      {
+        std::lock_guard<std::mutex> lock(frame_mutex);
+        frames.push_back(frame);
+      },
+      [&](const ros_ntrip_client::StatusEvent& status)
+      {
+        std::lock_guard<std::mutex> lock(status_mutex);
+        statuses.push_back(status.message);
+        std::lock_guard<std::mutex> code_lock(code_mutex);
+        codes.push_back(status.code);
+      }));
+
+  ASSERT_TRUE(waitForPredicate([&]()
+                               {
+                                 std::lock_guard<std::mutex> lock(frame_mutex);
+                                 return !frames.empty();
+                               }));
+
+  client.stop();
+  caster.stop();
+
+  const std::vector<StatusCode> copied_codes = copyStatusCodes(codes, code_mutex);
+  EXPECT_TRUE(containsStatusCode(copied_codes, StatusCode::TlsEstablished));
+  EXPECT_TRUE(containsStatusCode(copied_codes, StatusCode::SessionAccepted));
+  EXPECT_TRUE(containsStatusCode(copied_codes, StatusCode::StreamActive));
+
+  std::lock_guard<std::mutex> lock(frame_mutex);
+  ASSERT_FALSE(frames.empty());
+  EXPECT_EQ(frames.front(), kMinimalRtcmFrame);
+}
+
+TEST(NtripClientTest, FailsTlsHostnameVerificationWithWrongServerName)
+{
+  TempFile ca_certificate(kTlsServerCertificatePem);
+  TlsMockCaster caster(
+      kTlsServerCertificatePem,
+      kTlsServerPrivateKeyPem,
+      [](SSL*)
+      {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      });
+
+  NtripClientConfig config = makeConfig(caster.port());
+  config.host = "127.0.0.1";
+  config.tls_enabled = true;
+  config.tls_verify_peer = true;
+  config.tls_server_name = "wronghost";
+  config.tls_ca_cert_file = ca_certificate.path();
+  config.max_attempts = 1;
+  NtripClient client(config);
+
+  std::mutex status_mutex;
+  std::vector<std::string> statuses;
+  std::mutex code_mutex;
+  std::vector<StatusCode> codes;
+
+  ASSERT_TRUE(client.start(
+      [](const std::vector<std::uint8_t>&) {},
+      [&](const ros_ntrip_client::StatusEvent& status)
+      {
+        std::lock_guard<std::mutex> lock(status_mutex);
+        statuses.push_back(status.message);
+        std::lock_guard<std::mutex> code_lock(code_mutex);
+        codes.push_back(status.code);
+      }));
+
+  ASSERT_TRUE(waitForPredicate([&]()
+                               {
+                                 const std::vector<StatusCode> copied =
+                                     copyStatusCodes(codes, code_mutex);
+                                 return containsStatusCode(copied, StatusCode::TlsError);
+                               }));
+
+  client.stop();
+  caster.stop();
+
+  const std::vector<StatusCode> copied_codes = copyStatusCodes(codes, code_mutex);
+  EXPECT_TRUE(containsStatusCode(copied_codes, StatusCode::TlsError));
+  const std::vector<std::string> copied_statuses = copyStatuses(statuses, status_mutex);
+  EXPECT_TRUE(containsStatus(copied_statuses, "TLS"));
 }
 
 TEST(NtripClientTest, CountsRtcmCrcFailures)
